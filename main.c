4ed5eb0d7dfcc536daea2a78cd628b70bd8bfad9
tiffdefaria <tdefaria00@gmail.com>
2022-03-20 20:06:29 -0400

    main

--- main.c
+++ main.c
@@ -9,15 +9,15 @@ int main(int argc, char** argv){
         exit(1);
     }
     fseek(f, 0L, SEEK_END); //set the file position to the end of the file with 0L offset
-    int fsize = ftell(f); //the current file position relative to the beginning
+    int size = ftell(f); //the current file position relative to the beginning
     fseek(f, 0L, SEEK_SET); //set the file position to the beginning of the file with 0L offset
 
-    unsigned char *buffer = malloc(fsize); //allocate memory for the specified file size
+    unsigned char *buffer = malloc(size); //allocate memory for the specified file size
     fclose(f); //closes the file stream
 
     //Program counter functionality
     int pc = 0;
-    while(pc < fsize){
+    while(pc < size){
         pc += disassemble8080(buffer, pc); //increment the program counter with the instruction size
     }
     return 0;
